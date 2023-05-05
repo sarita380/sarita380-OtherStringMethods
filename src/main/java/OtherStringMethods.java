3ad6b3fb0b96315fd1619f3ebbd754922ff5e640
Sara Aliaga <sarialiaga5@gmail.com>
2023-05-05 02:51:01 +0000

    Completed Lab: Other String Methods

--- src/main/java/OtherStringMethods.java
+++ src/main/java/OtherStringMethods.java
@@ -11,7 +11,8 @@ public class OtherStringMethods {
      * @return
      */
     public String partOfString(String s, int start, int end){
-        return "";
+        String partOfStr = s.substring(start, end);
+        return partOfStr;
     }
 
     /**
@@ -26,7 +27,8 @@ public class OtherStringMethods {
      * and 0 if s1 is equivalent to s2.
      */
     public int compareLexigraphically(String s1, String s2) {
-        return 0;
+           int compare =  s1.compareTo(s2);
+        return compare;
     }
 
     /**
@@ -39,6 +41,8 @@ public class OtherStringMethods {
      * @return an array of Strings that represent s1 split by splitAround.
      */
     public String[] splitStringIntoMultipleStrings(String s1, String splitAround) {
-        return null;
+        String [] arrStr = s1.split(splitAround);
+
+        return arrStr;
     }
 }
